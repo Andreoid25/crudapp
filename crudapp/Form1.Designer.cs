267e4edd80a05117a690d42241f6d283296fcdca
Andreoid25 <Andremndnts@gmail.com>
2024-06-27 15:59:58 +0800

    initial commit

---
+++ crudapp/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace crudapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.carBrandInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.carColor = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.carPayment = new System.Windows.Forms.TextBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnEdit = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Car Brand";
+            // 
+            // carBrandInput
+            // 
+            this.carBrandInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carBrandInput.Location = new System.Drawing.Point(74, 111);
+            this.carBrandInput.Name = "carBrandInput";
+            this.carBrandInput.Size = new System.Drawing.Size(159, 29);
+            this.carBrandInput.TabIndex = 2;
+            this.carBrandInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(127, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Color";
+            // 
+            // carColor
+            // 
+            this.carColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carColor.Location = new System.Drawing.Point(74, 161);
+            this.carColor.Name = "carColor";
+            this.carColor.Size = new System.Drawing.Size(159, 29);
+            this.carColor.TabIndex = 4;
+            this.carColor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(117, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Payment";
+            // 
+            // carPayment
+            // 
+            this.carPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carPayment.Location = new System.Drawing.Point(74, 209);
+            this.carPayment.Name = "carPayment";
+            this.carPayment.Size = new System.Drawing.Size(159, 29);
+            this.carPayment.TabIndex = 6;
+            this.carPayment.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(287, 62);
+            this.DataGridView1.MultiSelect = false;
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(442, 217);
+            this.DataGridView1.TabIndex = 8;
+            this.DataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAdd.Location = new System.Drawing.Point(97, 267);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(104, 33);
+            this.BtnAdd.TabIndex = 9;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = false;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnEdit
+            // 
+            this.BtnEdit.BackColor = System.Drawing.Color.Lime;
+            this.BtnEdit.Enabled = false;
+            this.BtnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEdit.Location = new System.Drawing.Point(401, 309);
+            this.BtnEdit.Name = "BtnEdit";
+            this.BtnEdit.Size = new System.Drawing.Size(104, 33);
+            this.BtnEdit.TabIndex = 10;
+            this.BtnEdit.Text = "Edit";
+            this.BtnEdit.UseVisualStyleBackColor = false;
+            this.BtnEdit.Click += new System.EventHandler(this.BtnEdit_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.BackColor = System.Drawing.Color.Red;
+            this.BtnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDelete.Location = new System.Drawing.Point(535, 309);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(104, 33);
+            this.BtnDelete.TabIndex = 11;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = false;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSave.Location = new System.Drawing.Point(97, 267);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(104, 33);
+            this.BtnSave.TabIndex = 12;
+            this.BtnSave.Text = "Save Changes";
+            this.BtnSave.UseVisualStyleBackColor = false;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(395, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(223, 36);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "CAR CRUD APP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnEdit);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.carPayment);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.carColor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.carBrandInput);
+            this.Controls.Add(this.BtnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox carBrandInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox carColor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox carPayment;
+        private System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnEdit;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ crudapp/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace crudapp
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dataTable; // DataTable to hold the data from car_details
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            BtnEdit.Enabled = false; // Disable Edit button by default
+            DataGridView1.SelectionChanged += DataGridView1_SelectionChanged; // Attach selection changed event
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+            string connectionString = "server=localhost; user id=root; password=; database=crudapp;";
+            string query = "SELECT * FROM car_details";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection))
+                {
+                    dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    DataGridView1.DataSource = dataTable;
+                }
+            }
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // Validate input fields
+            if (string.IsNullOrWhiteSpace(carBrandInput.Text) ||
+                string.IsNullOrWhiteSpace(carColor.Text) ||
+                string.IsNullOrWhiteSpace(carPayment.Text))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            string connectionString = "server=localhost; user id=root; password=; database=crudapp;";
+            string insertQuery = "INSERT INTO car_details (carBrand, carColor, payment) VALUES (@value1, @value2, @value3)";
+            string selectQuery = "SELECT * FROM car_details WHERE id = LAST_INSERT_ID()";
+
+            string value1 = carBrandInput.Text;
+            string value2 = carColor.Text;
+            string value3 = carPayment.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@value1", value1);
+                    insertCommand.Parameters.AddWithValue("@value2", value2);
+                    insertCommand.Parameters.AddWithValue("@value3", value3);
+
+                    connection.Open();
+                    int result = insertCommand.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Record added successfully.");
+
+                        // Fetch the newly inserted row
+                        MySqlCommand selectCommand = new MySqlCommand(selectQuery, connection);
+                        using (MySqlDataReader reader = selectCommand.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Create a new DataRow with the retrieved data
+                                DataRow newRow = dataTable.NewRow();
+                                newRow["id"] = reader["id"];
+                                newRow["carBrand"] = reader["carBrand"];
+                                newRow["carColor"] = reader["carColor"];
+                                newRow["payment"] = reader["payment"];
+                                dataTable.Rows.Add(newRow);
+                            }
+                        }
+
+                        // Clear input fields after adding
+                        carBrandInput.Text = "";
+                        carColor.Text = "";
+                        carPayment.Text = "";
+
+                        // Refresh the DataGridView display
+                        DataGridView1.Refresh();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error adding record.");
+                    }
+                }
+            }
+        }
+
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            // Check if a row is selected (should be redundant due to button being disabled if no row is selected)
+            if (DataGridView1.SelectedRows.Count > 0)
+            {
+                // Get the selected row
+                DataGridViewRow selectedRow = DataGridView1.SelectedRows[0];
+
+                // Populate input fields with data from selected row
+                carBrandInput.Text = selectedRow.Cells["carBrand"].Value.ToString();
+                carColor.Text = selectedRow.Cells["carColor"].Value.ToString();
+                carPayment.Text = selectedRow.Cells["payment"].Value.ToString();
+
+                // Enable BtnSave
+                BtnSave.Visible = true;
+
+                BtnAdd.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to edit.");
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (DataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = DataGridView1.SelectedRows[0];
+                int id = Convert.ToInt32(selectedRow.Cells["id"].Value);
+
+                string value1 = carBrandInput.Text;
+                string value2 = carColor.Text;
+                string value3 = carPayment.Text;
+
+                string connectionString = "server=localhost; user id=root; password=; database=crudapp;";
+                string updateQuery = "UPDATE car_details SET carBrand = @carBrand, carColor = @carColor, payment = @payment WHERE id = @id";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    using (MySqlCommand command = new MySqlCommand(updateQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        command.Parameters.AddWithValue("@carBrand", value1);
+                        command.Parameters.AddWithValue("@carColor", value2);
+                        command.Parameters.AddWithValue("@payment", value3);
+
+                        connection.Open();
+                        int result = command.ExecuteNonQuery();
+
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Record updated successfully.");
+                            carBrandInput.Text = "";
+                            carColor.Text = "";
+                            carPayment.Text = "";
+
+                            BtnSave.Visible = false;
+
+                            DisplayData(); // Refresh DataGridView
+
+                            BtnAdd.Visible = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            // Check if a row is selected
+            if (DataGridView1.SelectedRows.Count > 0)
+            {
+                // Get the selected row
+                DataGridViewRow selectedRow = DataGridView1.SelectedRows[0];
+
+                // Get the ID of the selected row
+                int id = Convert.ToInt32(selectedRow.Cells["id"].Value);
+
+                // Confirmation dialog
+                DialogResult result = MessageBox.Show("Are you sure you want to delete this record?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Delete query
+                    string connectionString = "server=localhost; user id=root; password=; database=crudapp;";
+                    string deleteQuery = "DELETE FROM car_details WHERE id = @id";
+
+                    using (MySqlConnection connection = new MySqlConnection(connectionString))
+                    {
+                        using (MySqlCommand command = new MySqlCommand(deleteQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@id", id);
+
+                            connection.Open();
+                            int rowsAffected = command.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Record deleted successfully.");
+
+                                // Refresh DataGridView
+                                DisplayData();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error deleting record.");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+        }
+
+
+        private void DataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            // Enable BtnEdit if a row is selected
+            BtnEdit.Enabled = DataGridView1.SelectedRows.Count > 0;
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Handle the cell click event if needed
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
